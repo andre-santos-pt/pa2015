5e47d3a3d29ad2e9ce1385628d8774894b8e6c0f
miguelnobre <miguelnobre@outlook.com>
2015-11-10 18:50:38 +0000

    Internal Structure

---
+++ pt.iscte.pidesco.javadoc/src/pt/iscte/pidesco/javadoc/internal/Activator.java
@@ -0,0 +1,17 @@
+package pt.iscte.pidesco.javadoc.internal;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		
+	}
+}

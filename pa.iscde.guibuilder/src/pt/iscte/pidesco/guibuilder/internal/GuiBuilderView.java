437ab9c90ed752eae2176c66acb46e8908cd71ae
rudiluis <rudi121_kemo@hotmail.com>
2015-11-11 00:53:52 +0000

    New TabFolder

--- pa.iscde.guibuilder/src/pt/iscte/pidesco/guibuilder/internal/GuiBuilderView.java
+++ pa.iscde.guibuilder/src/pt/iscte/pidesco/guibuilder/internal/GuiBuilderView.java
@@ -10,6 +10,7 @@ import org.eclipse.draw2d.XYLayout;
 import org.eclipse.draw2d.geometry.Dimension;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.SashForm;
+import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.FillLayout;
@@ -29,7 +30,6 @@ import pt.iscte.pidesco.guibuilder.ui.FigureMoverResizer;
 public class GuiBuilderView implements PidescoView {
 	private Composite topComposite;
 	private Composite bottomComposite;
-	
 
 	public GuiBuilderView() {
 		// TODO Auto-generated constructor stub
@@ -40,21 +40,21 @@ public class GuiBuilderView implements PidescoView {
 		createViewTemporarySolution(viewArea, imageMap);
 		populateTopComposite(topComposite);
 		populateBottomComposite(bottomComposite);
-		
+
 		// viewArea.setLayout(new RowLayout());
-//
-//		 final Label label = new Label(viewArea, SWT.NONE);
-//		 label.setText("text on the label");
-//		
-//		 //label.setImage(getCheckedImage(label.getDisplay()));
-//		 label.setImage(imageMap.get("frame.png"));
-//		
-//		 addTabbar(viewArea);
-//		
-//		 addPopUpMenu(label);
-//		
-//		
-//		 addFigDragDrop(viewArea);
+		//
+		// final Label label = new Label(viewArea, SWT.NONE);
+		// label.setText("text on the label");
+		//
+		// //label.setImage(getCheckedImage(label.getDisplay()));
+		// label.setImage(imageMap.get("frame.png"));
+		//
+		// addTabbar(viewArea);
+		//
+		// addPopUpMenu(label);
+		//
+		//
+		// addFigDragDrop(viewArea);
 
 		// Create the tree and some tree items
 		// final Tree tree = new Tree(viewArea, SWT.NONE);
@@ -182,18 +182,18 @@ public class GuiBuilderView implements PidescoView {
 
 		// Create the SashForm with VERTICAL
 		SashForm sashForm = new SashForm(viewArea, SWT.VERTICAL);
-		
+
 		topComposite = new Composite(sashForm, SWT.BORDER);
 		topComposite.setLayout(new FillLayout());
-		
+
 		bottomComposite = new Composite(sashForm, SWT.BORDER);
 		bottomComposite.setLayout(new FillLayout());
-	
+
 		// Define the relation between both composites
 		sashForm.setWeights(new int[] { 4, 1 });
 	}
-	
-	private void populateTopComposite(Composite composite){
+
+	private void populateTopComposite(Composite composite) {
 		Canvas canvas = new Canvas(composite, SWT.NONE);
 		LightweightSystem lws = new LightweightSystem(canvas);
 		Figure contents = new Figure();
@@ -217,28 +217,92 @@ public class GuiBuilderView implements PidescoView {
 		contents.add(fig);
 		contents.add(fig2);
 
-		
-		
-		
 	}
-	
-	private void populateBottomComposite(Composite composite){
+
+	private void populateBottomComposite(Composite composite) {
+
 		final TabFolder tabFolder = new TabFolder(composite, SWT.TOP);
 
-		for (int loopIndex = 0; loopIndex < 10; loopIndex++) {
+		for (int loopIndex = 0; loopIndex < 3; loopIndex++) {
 			TabItem tabItem = new TabItem(tabFolder, SWT.NULL);
-			tabItem.setText("Tab " + loopIndex);
 
-			// Text text = new Text(tabFolder, SWT.BORDER);
-			// text.setText("This is page " + loopIndex);
-			// tabItem.setControl(text);
-			final Button button = new Button(tabFolder, SWT.FLAT);
-			button.setText("Button");
-			button.setAlignment(SWT.CENTER);
-			// button.setImage(new Image(viewArea.getDisplay(), "frame.png"));
-			tabItem.setControl(button);
+			final ScrolledComposite sci = new ScrolledComposite(tabFolder, SWT.BORDER | SWT.H_SCROLL);
+			Composite compositeButtons = new Composite(sci, SWT.NONE);
+			compositeButtons.setLayout(new FillLayout());
 
+			sci.setContent(compositeButtons);
+ 
+			tabItem.setControl(sci);
+
+			switch (loopIndex) {
+			case 0:
+				tabItem.setText("Components");
+
+				for (int t = 0; t <= 4; t++) {
+					Button button = new Button(compositeButtons, SWT.PUSH);
+					button.setAlignment(SWT.CENTER);
+
+					switch (t) {
+					case 0:
+						button.setText("JButton");
+						break;
+					case 1:
+						button.setText("JLabel");
+						break;
+					case 2:
+						button.setText("JTextField");
+						break;
+					case 3:
+						button.setText("JRadioButton");
+						break;
+					case 4:
+						button.setText("JCheckBox");
+						break;
+
+					default:
+						break;
+					}
+				}
+				compositeButtons.setSize(500, 90);
+				break;
+
+			case 1:
+				tabItem.setText("Layouts");
+
+				for (int t = 0; t < 3; t++) {
+					Button button = new Button(compositeButtons, SWT.PUSH);
+					button.setAlignment(SWT.CENTER);
+
+					switch (t) {
+					case 0:
+						button.setText("Flow \n Layout");
+						break;
+					case 1:
+						button.setText("Absolute \n Layout");
+						break;
+					case 2:
+						button.setText("Border \n Layout");
+						break;
+
+					default:
+						break;
+					}
+				}
+				compositeButtons.setSize(300, 90);
+				break;
+
+			case 2:
+				tabItem.setText("Containers");
+
+				Button button = new Button(compositeButtons, SWT.PUSH);
+				button.setAlignment(SWT.CENTER);
+				button.setText("JPanel");
+				compositeButtons.setSize(100, 90);
+				break;
+
+			default:
+				break;
+			}
 		}
-		tabFolder.setSize(400, 10);
 	}
 }

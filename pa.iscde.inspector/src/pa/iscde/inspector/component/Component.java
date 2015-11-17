8fbc44f352d84f36f82881ad87b7a4e6a60165c5
Ronaldo <Ronaldo@Ronaldo-TOSH>
2015-11-17 19:59:35 +0000

    [pa.iscte.inspector.component] add getters and setters

--- pa.iscde.inspector/src/pa/iscde/inspector/component/Component.java
+++ pa.iscde.inspector/src/pa/iscde/inspector/component/Component.java
@@ -1,5 +1,49 @@
 package pa.iscde.inspector.component;
 
-public class Component {
+import java.util.List;
 
+import org.osgi.framework.BundleContext;
+
+public class Component {
+	
+	private String plugin_name;
+	private List<Component> extension_Point;
+	private List<Component> extension;
+	private boolean active;
+	private BundleContext activator;
+	
+	
+	public String getPlugin_name() {
+		return plugin_name;
+	}
+	public void setPlugin_name(String plugin_name) {
+		this.plugin_name = plugin_name;
+	}
+	public List<Component> getExtension_Point() {
+		return extension_Point;
+	}
+	public void setExtension_Point(List<Component> extension_Point) {
+		this.extension_Point = extension_Point;
+	}
+	public List<Component> getExtension() {
+		return extension;
+	}
+	public void setExtension(List<Component> extension) {
+		this.extension = extension;
+	}
+	public boolean isActive() {
+		return active;
+	}
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	public BundleContext getActivator() {
+		return activator;
+	}
+	public void setActivator(BundleContext activator) {
+		this.activator = activator;
+	}
+	
+	
+	
 }
--- pa.iscde.inspector/src/pa/iscde/inspector/component/FileReadear.java
+++ pa.iscde.inspector/src/pa/iscde/inspector/component/FileReadear.java
@@ -1,8 +1,15 @@
 package pa.iscde.inspector.component;
 
+import java.io.File;
+
 public class FileReadear {
+
+	private File file;
 
 	public void getAllComponents(){
 		 
+		
+		
+		
 	}
 }

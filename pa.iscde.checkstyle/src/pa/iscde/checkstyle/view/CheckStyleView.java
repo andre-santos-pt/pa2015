41036f78bd456b1bce0f72c2c42a947aae9a92e3
joao-paulo-f-pires <joao.pires@iscte.pt>
2015-11-21 19:34:52 +0000

    Added Javadoc.

--- pa.iscde.checkstyle/src/pa/iscde/checkstyle/view/CheckStyleView.java
+++ pa.iscde.checkstyle/src/pa/iscde/checkstyle/view/CheckStyleView.java
@@ -1,5 +1,6 @@
 package pa.iscde.checkstyle.view;
 
+import java.io.File;
 import java.util.Map;
 
 import org.eclipse.jface.viewers.ArrayContentProvider;
@@ -19,39 +20,98 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
 
+import pa.iscde.checkstyle.internal.CheckStyleActivator;
 import pa.iscde.checkstyle.model.SeverityType;
 import pa.iscde.checkstyle.model.Violation;
 import pa.iscde.checkstyle.model.ViolationDetail;
 import pa.iscde.checkstyle.model.ViolationModelProvider;
 import pt.iscte.pidesco.extensibility.PidescoView;
 
+/**
+ * This class is responsible to render the main and detailed reports associated
+ * to Checkstyle analysis performed in the classes existing in the projects.
+ * 
+ * It is responsible too to handle the events performed in the main and detailed
+ * reports.
+ */
 public class CheckStyleView implements PidescoView {
 
-	private static final String[] COLUMN_NAMES_DETAIL_REPORT = { "Severity", "Resource", "Location", "Line", "Description" };
+	/**
+	 * The columns' names for detailed report.
+	 */
+	private static final String[] COLUMN_NAMES_DETAILED_REPORT = { "Severity", "Resource", "Location", "Line",
+			"Description" };
 
-	private static final int[] COLUMN_WIDTHS_DETAIL_REPORT = { 100, 200, 300, 100, 200 };
+	/**
+	 * The columns' widths for detailed report.
+	 */
+	private static final int[] COLUMN_WIDTHS_DETAILED_REPORT = { 100, 200, 300, 100, 200 };
 
-	private static final String[] COLUMN_NAMES = { "Severity", "Violation Type", "Count", "Description", "" };
+	/**
+	 * The columns' names for main report.
+	 */
+	private static final String[] COLUMN_NAMES_MAIN_REPORT = { "Severity", "Violation Type", "Count", "Description",
+			"" };
 
-	private static final int[] COLUMN_WIDTHS = { 100, 300, 100, 300, 0 };
+	/**
+	 * The columns' widths for main report.
+	 */
+	private static final int[] COLUMN_WIDTHS_MAIN_REPORT = { 100, 300, 100, 300, 0 };
 
+	/**
+	 * Singleton instance for this class loaded eagerly.
+	 */
 	private static CheckStyleView INSTANCE;
 
+	/**
+	 * The table viewer.
+	 */
 	private TableViewer viewer;
 
+	/**
+	 * The table used to present the main and detailed report.
+	 */
 	private Table table;
 
+	/**
+	 * The view are in which the main and detailed reports are rendered.
+	 */
 	private Composite viewArea;
 
+	/**
+	 * The auxiliary structures with mapping for all image resources existing in
+	 * the application.
+	 */
 	private Map<String, Image> imageMap;
 
+	/**
+	 * The navigation button use to navigate from detailed report to main
+	 * report.
+	 */
 	private Button btMainReport;
 
+	/**
+	 * The navigation button use to navigate from main report to detailed
+	 * report.
+	 */
 	private Button btDetailReport;
 
+	/**
+	 * Holds the violation object associated to the row selected in the main
+	 * report.
+	 */
 	private Violation selectedViolation;
-	
-	private boolean isDetailReportOpened;
+
+	/**
+	 * Holds the violation detail object associated to the row selected in the
+	 * detailed report.
+	 */
+	private ViolationDetail selectedViolationDetail;
+
+	/**
+	 * Used to validate if the detailed report is being used.
+	 */
+	private boolean isDetailedReportOpened;
 
 	@Override
 	public void createContents(Composite viewArea, Map<String, Image> imageMap) {
@@ -62,52 +122,49 @@ public class CheckStyleView implements PidescoView {
 	}
 
 	/**
-	 * TODO
+	 * This method is used to return the singleton instance of this class.
 	 * 
-	 * @return
+	 * @return The singleton instance.
 	 */
 	public static CheckStyleView getInstance() {
 		return INSTANCE;
 	}
 
 	/**
-	 * TODO
+	 * This method is used to update the data model used by main report. If the
+	 * detailed report is being used, the data model for main report will not be
+	 * updated.
 	 */
 	public void updateModel() {
-		if(isDetailReportOpened){
+		if (isDetailedReportOpened) {
 			return;
 		}
-		
+
 		if (!viewArea.isDisposed()) {
 			viewer.setInput(ViolationModelProvider.getInstance().getViolations());
 		}
 	}
 
 	/**
-	 * TODO
-	 * 
-	 * @param
+	 * This method is used to update the data model used by detailed report.
 	 */
-	private void updateDetailReportModel() {
+	private void updateDetailedReportModel() {
 		if (!viewArea.isDisposed()) {
 			viewer.setInput(selectedViolation.getDetails());
 		}
 	}
 
 	/**
-	 * TODO
-	 * 
-	 * @param viewArea
-	 * @return
+	 * This method is responsible to create the skeleton of the views for main
+	 * and detailed reports and add the needed listeners in order to handle the
+	 * actions performed on them.
 	 */
 	private void render() {
-		isDetailReportOpened = false;
+		isDetailedReportOpened = false;
+
 		addNavigationButtons();
 		addNavigationButtonsListeners();
 
-		// SWT.SINGLE - Only one row is selected in the table
-		// This is useful when we want to analyze the violation for a specific
-		// violation type.
 		viewer = new TableViewer(viewArea, SWT.SINGLE | SWT.H_SCROLL | SWT.V_SCROLL | SWT.FULL_SELECTION | SWT.BORDER);
 		addViewerListener();
 
@@ -121,48 +178,12 @@ public class CheckStyleView implements PidescoView {
 		viewer.setContentProvider(new ArrayContentProvider());
 		updateModel();
 
-		final GridData gridData = new GridData();
-		gridData.verticalAlignment = GridData.FILL;
-		gridData.horizontalSpan = 3;
-		gridData.grabExcessHorizontalSpace = true;
-		gridData.grabExcessVerticalSpace = true;
-		gridData.horizontalAlignment = GridData.FILL;
-
-		viewer.getControl().setLayoutData(gridData);
-	}
-
-	private void addNavigationButtonsListeners() {
-		btMainReport.addSelectionListener(new SelectionListener() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				isDetailReportOpened = false;
-				updateModel();
-				btDetailReport.setEnabled(true);
-				btMainReport.setEnabled(false);
-			}
-			
-			@Override
-			public void widgetDefaultSelected(SelectionEvent e) {
-				// Nothing to do.
-			}
-		});
-		
-		btDetailReport.addSelectionListener(new SelectionListener() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				//TODO
-				System.out.println(e);
-			}
-			
-			@Override
-			public void widgetDefaultSelected(SelectionEvent e) {
-				// Nothing to do.
-			}
-		});
+		viewer.getControl().setLayoutData(new GridData(GridData.FILL, GridData.FILL, true, true, 3, 0));
 	}
 
 	/**
-	 * TODO
+	 * This method is used to create the navigation buttons used to navigate
+	 * between the main report to detailed report and vice-versa.
 	 */
 	private void addNavigationButtons() {
 		final GridLayout layout = new GridLayout(2, false);
@@ -172,55 +193,126 @@ public class CheckStyleView implements PidescoView {
 		btMainReport.setBounds(40, 40, 40, 40);
 		btMainReport.setImage(imageMap.get("nav_main_report.png"));
 		btMainReport.setEnabled(false);
+		btMainReport.setLayoutData(new GridData(SWT.RIGHT, SWT.NONE, false, false, 0, 0));
+		btMainReport.setToolTipText("Go back to the master view");
 
 		btDetailReport = new Button(viewArea, SWT.PUSH);
 		btDetailReport.setBounds(40, 40, 40, 40);
 		btDetailReport.setImage(imageMap.get("nav_detailed_report.png"));
 		btDetailReport.setEnabled(false);
+		btDetailReport.setLayoutData(new GridData(SWT.RIGHT, SWT.NONE, false, false, 1, 1));
+		btDetailReport.setToolTipText("List all the violations of this type");
 	}
 
 	/**
-	 * TODO
+	 * This method is used to add the listeners to handle the actions performed
+	 * on the navigation buttons.
 	 */
-	private void updateColumns() {
+	private void addNavigationButtonsListeners() {
+		btMainReport.addSelectionListener(new SelectionListener() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				isDetailedReportOpened = false;
+				updateModel();
+				btDetailReport.setEnabled(selectedViolation != null);
+				btMainReport.setEnabled(false);
+			}
+
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				// Nothing to do.
+			}
+		});
+
+		btDetailReport.addSelectionListener(new SelectionListener() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				isDetailedReportOpened = true;
+				updateDetailedReportModel();
+				btDetailReport.setEnabled(false);
+				btMainReport.setEnabled(true);
+				selectedViolation = null;
+			}
+
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				// Nothing to do.
+			}
+		});
+	}
+
+	/**
+	 * This method is used to update the columns' names and widths between the
+	 * visualization between the main report and the detailed report.
+	 */
+	private void updateColumns(boolean isMainReport) {
 		final TableColumn[] columns = viewer.getTable().getColumns();
-		for (int i = 0; i < columns.length; ++i) {
-			columns[i].setText(COLUMN_NAMES_DETAIL_REPORT[i]);
-			columns[i].setWidth(COLUMN_WIDTHS_DETAIL_REPORT[i]);
+
+		if (!isMainReport) {
+			for (int i = 0; i < columns.length; ++i) {
+				columns[i].setText(COLUMN_NAMES_DETAILED_REPORT[i]);
+				columns[i].setWidth(COLUMN_WIDTHS_DETAILED_REPORT[i]);
+			}
+		} else {
+			for (int i = 0; i < columns.length; ++i) {
+				columns[i].setText(COLUMN_NAMES_DETAILED_REPORT[i]);
+				columns[i].setWidth(COLUMN_WIDTHS_DETAILED_REPORT[i]);
+			}
 		}
 	}
 
 	/**
+	 * This method is used to add a listener to handle the double click event on
+	 * the rows on the table associated to main and detailed reports.
 	 * 
+	 * If the selected row belongs to main report, the double click event will
+	 * open the detail report to present the detail of violations for linked to
+	 * the violation type associated to the selected row.
+	 * 
+	 * If the selected row belongs belongs to detailed report, the double click
+	 * event will open the file in which the violation was detected, selecting
+	 * also the text associated to the mentioned violation.
 	 */
 	private void addViewerListener() {
 		viewer.addDoubleClickListener(new IDoubleClickListener() {
 			@Override
 			public void doubleClick(DoubleClickEvent e) {
-				updateColumns();
-				updateDetailReportModel();
-				btDetailReport.setEnabled(false);
-				btMainReport.setEnabled(true);
-				isDetailReportOpened = true;
+				if (!isDetailedReportOpened) {
+					updateColumns(false);
+					updateDetailedReportModel();
+					btDetailReport.setEnabled(false);
+					btMainReport.setEnabled(true);
+					isDetailedReportOpened = true;
+					selectedViolation = null;
+				} else {
+					CheckStyleActivator.getInstance().getJavaEditorServices().selectText(
+							new File(selectedViolationDetail.getLocation()), selectedViolationDetail.getOffset(), 0);
+				}
 			}
 		});
 	}
 
 	/**
 	 * This listener will be used to handle the selection event performed on the
-	 * rows violation main view. Based on the selected row, we can retrieve the
-	 * corresponding violation object and also the corresponding violation
-	 * details.
-	 * 
-	 * The single selection
+	 * rows violation main and detail view. Based on the selected row, we can
+	 * retrieve the corresponding violation object and also the corresponding
+	 * violation details and vice-versa.
 	 */
 	private void addTableListener() {
 		table.addSelectionListener(new SelectionListener() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				selectedViolation = (Violation) e.item.getData();
-				btDetailReport.setEnabled(true);
-				btMainReport.setEnabled(false);
+				if (e.item.getData() instanceof Violation) {
+					selectedViolation = (Violation) e.item.getData();
+					btDetailReport.setEnabled(true);
+					btMainReport.setEnabled(false);
+				}
+
+				if (e.item.getData() instanceof ViolationDetail) {
+					selectedViolationDetail = (ViolationDetail) e.item.getData();
+					btDetailReport.setEnabled(false);
+					btMainReport.setEnabled(true);
+				}
 			}
 
 			@Override
@@ -231,10 +323,12 @@ public class CheckStyleView implements PidescoView {
 	}
 
 	/**
-	 * TODO
+	 * This method is used to create the columns for the main report and add the
+	 * corresponding label providers, in order to populate the table data to be
+	 * present, either for main and detailed report.
 	 */
 	private void createColumns() {
-		TableViewerColumn col = createTableViewerColumn(COLUMN_NAMES[0], COLUMN_WIDTHS[0], 0);
+		TableViewerColumn col = createTableViewerColumn(COLUMN_NAMES_MAIN_REPORT[0], COLUMN_WIDTHS_MAIN_REPORT[0], 0);
 		col.setLabelProvider(new ColumnLabelProvider() {
 			@Override
 			public String getText(Object element) {
@@ -289,7 +383,7 @@ public class CheckStyleView implements PidescoView {
 			}
 		});
 
-		col = createTableViewerColumn(COLUMN_NAMES[1], COLUMN_WIDTHS[1], 1);
+		col = createTableViewerColumn(COLUMN_NAMES_MAIN_REPORT[1], COLUMN_WIDTHS_MAIN_REPORT[1], 1);
 		col.setLabelProvider(new ColumnLabelProvider() {
 			@Override
 			public String getText(Object element) {
@@ -303,7 +397,7 @@ public class CheckStyleView implements PidescoView {
 			}
 		});
 
-		col = createTableViewerColumn(COLUMN_NAMES[2], COLUMN_WIDTHS[2], 2);
+		col = createTableViewerColumn(COLUMN_NAMES_MAIN_REPORT[2], COLUMN_WIDTHS_MAIN_REPORT[2], 2);
 		col.setLabelProvider(new ColumnLabelProvider() {
 			@Override
 			public String getText(Object element) {
@@ -317,7 +411,7 @@ public class CheckStyleView implements PidescoView {
 			}
 		});
 
-		col = createTableViewerColumn(COLUMN_NAMES[3], COLUMN_WIDTHS[3], 3);
+		col = createTableViewerColumn(COLUMN_NAMES_MAIN_REPORT[3], COLUMN_WIDTHS_MAIN_REPORT[3], 3);
 		col.setLabelProvider(new ColumnLabelProvider() {
 			@Override
 			public String getText(Object element) {
@@ -331,7 +425,7 @@ public class CheckStyleView implements PidescoView {
 			}
 		});
 
-		col = createTableViewerColumn(COLUMN_NAMES[4], COLUMN_WIDTHS[4], 4);
+		col = createTableViewerColumn(COLUMN_NAMES_MAIN_REPORT[4], COLUMN_WIDTHS_MAIN_REPORT[4], 4);
 		col.setLabelProvider(new ColumnLabelProvider() {
 			@Override
 			public String getText(Object element) {
@@ -346,12 +440,16 @@ public class CheckStyleView implements PidescoView {
 	}
 
 	/**
-	 * TODO
+	 * This method is used to create a particular table column to be added to a
+	 * table.
 	 * 
 	 * @param title
+	 *            The column title.
 	 * @param width
+	 *            The column width.
 	 * @param colNumber
-	 * @return
+	 *            The column number.
+	 * @return An object representing a column.
 	 */
 	private TableViewerColumn createTableViewerColumn(String title, int width, final int colNumber) {
 		final TableViewerColumn viewerColumn = new TableViewerColumn(viewer, SWT.NONE);
